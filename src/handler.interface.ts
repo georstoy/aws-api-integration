defc28d4ab500b0e4a6346039f95c31bba10fa5e
georstoy <georstoy@gmail.com>
2020-01-07 11:03:13 +0200

    define wikitypes and parsing methods

--- src/handler.interface.ts
+++ src/handler.interface.ts
@@ -1,17 +1,113 @@
 export interface WikiJson {
-    parse: {
-        title: string,
-        pageid: number,
-        wikitext: {
-            "*": string
-        }
+  parse: {
+    title: string;
+    pageid: number;
+    wikitext: {
+      "*": string;
+    };
+  };
+}
+
+export interface WikiPage {
+  title: string;
+  pageid: number;
+  wikitext: string;
+  distilledtext: string;
+}
+
+export class WikiTemplate {
+  static readonly regexp: RegExp = /\{\{([^\{]*?)\}\}/;
+  private text: string;
+
+  constructor(text: string) {
+    this.text = text;
+  }
+}
+
+export class WikiStyle {
+  static readonly regexp: RegExp = /[\']{2,4}/;
+}
+
+export class Link {
+  static readonly regexp: RegExp = /[\[]{2}(.*?)[\]]{2}/;
+  readonly text: string;
+  readonly url: string;
+
+  constructor(text: string, url: string) {
+    this.text = text;
+    this.url = url;
+  }
+
+}
+
+abstract class Reference {
+
+}
+export class ReferenceClosed extends Reference {
+ static readonly regexp: RegExp = /<ref[^<](*?)>.*?<\/ref>/;
+}
+
+export class ReferenceSelfClosing extends Reference {
+  static readonly regexp: RegExp = /<ref[^<](*?)\/>/;
+}
+ 
+export class WikiText {
+  private body: string;
+
+  constructor(text: string) {
+    this.body = text;
+  };
+
+  public remove = (regexp: RegExp): void => {
+    this.body.replace(regexp, "");
+  };
+
+  public removeAll = (regexp: RegExp): void => {
+    while (this.body.match(regexp)) {
+      this.remove(regexp);
     }
-}
+  };
 
-export interface WikiText {
+  // all classes that are extracted with this function
+  // MUST have a group definition in their regexp
+  public extractAll = (regexp: RegExp, items: Array<any>): Array<WikiTemplate> => {
+    while (this.body.match(regexp)) {
+      items.push(this.body.match(regexp)![1]);
+      this.remove(new RegExp(this.body.match(regexp)![0]));
+    };
+    return items;
+  };
 
-}
+  
+};
 
-export interface wikiPageHandlerResponse {
-    
-}
+export class WikiSection {
+  private wikitext: WikiText;
+  private templates: Array<WikiTemplate>;
+  private links: Array<Link>;
+  private references: Array<Reference>;
+
+  constructor(text: string) {
+    this.wikitext = new WikiText(text);
+  }
+
+  public extractTemplates = (): void => {
+    this.wikitext.extractAll(WikiTemplate.regexp, this.templates);
+  };
+
+  public removeStyles = (): void => {
+    this.wikitext.removeAll(WikiStyle.regexp);
+  };
+
+  public extractLinks = (): void => {
+    this.wikitext.extractAll(Link.regexp, this.links);
+  };
+
+  public extractReferences = (): void => {
+    this.wikitext.extractAll(ReferenceClosed.regexp, this.references);
+    this.wikitext.extractAll(ReferenceSelfClosing.regexp, this.references);
+  };
+
+};
+  
+
--- src/handler.ts
+++ src/handler.ts
@@ -5,76 +5,37 @@ import { Response } from "node-fetch";
 import {
   WikiJson,
   WikiText,
-  wikiPageHandlerResponse
+  WikiSection,
+  WikiPage
 } from "./handler.interface";
-import { SpaceDelimitedTextPattern } from "@aws-cdk/aws-logs";
 
 const wikiPageHandler: Handler = async (
   event: any,
-  context: Context) => {
+  context: Context
+  ): Promise<WikiPage> => {
     const pageTag = event.queryStringParameters.page_tag;
     const lang = event.queryStringParameters.language;
     const url: string = `https://${lang}.wikipedia.org/w/api.php?action=parse&page=${pageTag}&prop=wikitext&format=json`;
   
     return new Promise( (resolve,reject) => {
       fetch(url)
-        .then(response => response.json())
-        .then(body => {
-          resolve(body);
+        .then(res => res.json())
+        .then( (body: WikiJson) => {
+          console.log(body);
+          
+
+          resolve({
+            title: body.parse.title,
+            pageid: body.parse.pageid,
+            wikitext: body.parse.wikitext['*'],
+            distilledtext: 'TODO'
+          });
         })
         .catch(err => {
           reject(err);
         });
     });
     
-  // Place for query parameters internal valdation
-/*
-  // fetch wikipage
-  const url: string = `https://${lang}.wikipedia.org/w/api.php?action=parse&page=${pageTag}&prop=wikitext&format=json`;
-  let body: string;
-  let statusCode: number;
-  let response: wikiPageHandlerResponse;
-
-  console.log(`fetch ${url}`);
-  
-  /*
-  fetch(url)
-    .then((res: Response) => {
-      console.log('Got Wiki response');
-      if (!res.ok) {
-        // Page not found
-        console.log('Bad Wiki response!')
-        throw new Error(res.statusText);
-      }
-      return res.json();
-    })
-    .then((resJson: WikiJson) => {
-      console.log(`response json ${resJson}`);
-      callback(null, {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*" // Required for CORS support to work
-        },
-        body: JSON.stringify({
-          title: resJson.parse.title,
-          pageid: resJson.parse.pageid,
-          //distiledtext: this.parseWikiPageToJson(resJson.parse.wikitext["*"]),
-          wikitext: resJson.parse.wikitext["*"]
-        })
-      });
-    })
-    .catch((error: any) => {
-      callback(null, {
-        statusCode: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*" // Required for CORS support to work
-        },
-        body: JSON.stringify({
-          error
-        })
-      });
-    });
-  */
 };
 
 const defaultHandler: Handler = (
